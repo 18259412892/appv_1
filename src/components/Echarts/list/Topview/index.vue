009267f64ae17f3457f74e4463e191dfd78599b8
zhangyunjiang <18259412892@163.com>
2020-11-10 23:05:29 +0800

    累计订单量 累计销售额

--- src/components/Echarts/list/Topview/index.vue
+++ src/components/Echarts/list/Topview/index.vue
@@ -4,20 +4,35 @@
         <a-col :span="6">
             <Card title="累计销售额" value="￥ 32,039,165">
                 <template v-slot:chart>
-                    <div class="compare"></div>
+                    <div class="compare-wrapper">
+                        <div class="compare">
+                            <span>日同比</span>
+                            <span class="emphasis">7.33%</span>
+                            <div class="increase" />
+                        </div>
+                        <div class="compare">
+                            <span>月同比</span>
+                            <span class="emphasis">7.33%</span>
+                            <div class="decrease" />
+                        </div>
+                    </div>
+
                 </template>
                 <template v-slot:footer>
                     <span>昨日销售额</span>
-                    <span class="money">￥30,000,000</span>
+                    <span class="emphasis">￥30,000,000</span>
                 </template>
             </Card>
         </a-col>
         <a-col :span="6">
             <Card title="累计订单量" value="2,115,165">
                 <template v-slot:chart>
-                    内容
+                    <div id="tital-order-chart" :style="{width:'100%',height:'100%'}" />
                 </template>
-
+                <template v-slot:footer>
+                    <span>昨日订单量</span>
+                    <span class="emphasis">20,000,000</span>
+                </template>
             </Card>
         </a-col>
         <a-col :span="6">
@@ -43,6 +58,46 @@ import Card from '../../../Card/card'
 export default {
     components: {
         Card
+    },
+    mounted() {
+        const chartDom = document.querySelector('#tital-order-chart');
+        const chart = this.$echarts.init(chartDom, 'wonderland');
+        const options = {
+            xAxis: {
+                type: "category",
+                show: false,
+                boundaryGap: false, //宽度极限拉伸
+            },
+            yAxis: {
+                show: false
+            },
+            series: [{
+                type: 'line',
+                data: [
+                    640, 432, 220, 534, 790,
+                    430, 220, 320, 532, 320, 834,
+                    430, 220, 320, 532, 320, 834,
+                    690, 530, 220, 620
+                ],
+                areaStyle: {
+                    color: '#22c3aa'
+                },
+                lineStyle: { //线条宽度
+                    width: 0
+                },
+                itemStyle: { //节点小点点
+                    opacity: 0
+                },
+                smooth: true //是否平滑
+            }],
+            grid: {
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0
+            }
+        }
+        chart.setOption(options);
     }
 }
 </script>
